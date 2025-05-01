8dfe48a1c4b0f005c45c9f517c87dda022611bc6
nirobsarker012 <nirobsarker012@gmail.com>
2025-05-01 15:48:16 +0600

    added: header-section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div>
+      <main></main>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
+import { format } from "date-fns";
 import React from "react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header>
+        <nav className="container flex flex-col justify-center text-center py-2">
+          <h2 className="font-english text-[#444444] text-[60px] mb-4">
+            The Dragon News
+          </h2>
+          <span className="text-[18px] text-[#444444] mb-[10px]">
+            Journalism Without Fear or Favour
+          </span>
+          <p className="text-[#706F6F] text-[20px]">
+            <span className="text-[#403F3F]">
+              {format(new Date(), "EEEE")},{" "}
+            </span>
+            {format(new Date(), "MMMM ee, yyyy")}
+          </p>
+        </nav>
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;
