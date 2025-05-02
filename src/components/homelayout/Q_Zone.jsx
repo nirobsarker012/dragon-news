17534978640a700b4799bc9aed1cc36d0c8840b9
nirobsarker012 <nirobsarker012@gmail.com>
2025-05-02 21:56:43 +0600

    added: q-zone-section

--- src/components/homelayout/Q_Zone.jsx
+++ src/components/homelayout/Q_Zone.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import class_img from "../../assets/class.png";
+import play_img from "../../assets/playground.png";
+import swiming from "../../assets/swimming.png";
 
 const Q_Zone = () => {
-  return <div></div>;
+  return (
+    <div className="bg-[#F3F3F3] p-4 w-full">
+      <h2 className="font-semibold text-[20px] text-[#403F3F] mb-5">Q-Zone</h2>
+      {/* Image Div */}
+      <div>
+        <img src={class_img} alt="" />
+        <img src={play_img} alt="" />
+        <img src={swiming} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Q_Zone;
--- src/components/homelayout/RightAside.jsx
+++ src/components/homelayout/RightAside.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SocialLogin from "./SocialLogin";
 import FindUs from "./FindUs";
+import Q_Zone from "./Q_Zone";
 
 const RightAside = () => {
   return (
     <div className="space-y-5">
       <SocialLogin />
       <FindUs />
+      <Q_Zone />
     </div>
   );
 };
